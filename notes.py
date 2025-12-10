8ce0321042646ba1b289c6b4a90139f0a88a2bea
Alvin <alvintsa@buffalo.edu>
2025-12-10 13:34:58 -0500

    yes

--- notes.py
+++ notes.py
@@ -2,3 +2,9 @@ options.set_preference("media.eme.enabled", True)
 options.set_preference("media.gmp-widevinecdm.enabled", True)
 options.set_preference("media.gmp-manager.updateEnabled", True)
 options.set_preference("media.gmp-provider.enabled", True)
+
+options.set_preference("gfx.webrender.all", False)
+options.set_preference("media.hardware-video-decoding.enabled", False)
+
+options.set_preference("media.autoplay.default", 0)
+options.set_preference("media.autoplay.allow-muted", True)
