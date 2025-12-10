62b6c0724a34fcb0452465dbc39256ceca870842
Alvin <alvintsa@buffalo.edu>
2025-12-10 13:33:47 -0500

    yes

---
+++ notes.py
@@ -0,0 +1,4 @@
+options.set_preference("media.eme.enabled", True)
+options.set_preference("media.gmp-widevinecdm.enabled", True)
+options.set_preference("media.gmp-manager.updateEnabled", True)
+options.set_preference("media.gmp-provider.enabled", True)
